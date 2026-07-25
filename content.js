7f63aaba48731bf7b06845de69c653ae0b2b46b6
afro-architect <157778913+afro-architect@users.noreply.github.com>
2026-07-25 07:47:19 -0700

    Update content.js

--- content.js
+++ content.js
@@ -213,8 +213,9 @@ const LESSON_CONTENT = [
 },   
       { heading: "Recap", videoSrc: "assets/video/placeholder-course-intro.mp4", body: "By the end of this journey you'll have built your own working pages and mini projects and not just read about them." },
       { heading: "What's Next?", videoSrc: "assets/video/placeholder-course-intro.mp4", body: "By the end of this journey you'll have built your own working pages and mini projects and not just read about them." },
-]
-  },
+    ]
+  }
+},
   {
     id: "lesson-3",
     title: "What is HTML?",
